df3464c34140100dc3e0bc9d509529e6980e49ec
THANATHIPSTD <thanathip_suj@cmu.ac.th>
2025-06-26 21:07:30 +0700

    3.6

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const { createApp, ref } = Vue;
 
 createApp({
     setup(){
-        const product = ref('Chrome hearts T-shirt');
-        const description = ref('This is a limited edition Chrome Hearts T-shirt made from 100% cotton. It features the iconic Chrome Hearts logo and is available in various sizes.');
+        const product = ref('Socks');
+        const image = ref('./assets/images/socks_green.jpg');
+        const url = ref ('https://www.camt.cmu.ac.th'); 
+        
         return{
-            product,description
+            product,
+            image,
+            url
         }
     }
 }).mount('#app');
