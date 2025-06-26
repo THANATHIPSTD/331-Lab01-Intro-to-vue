60b9df5027ffe257a3c9f594584ad5f4dfe208c7
THANATHIPSTD <thanathip_suj@cmu.ac.th>
2025-06-26 22:27:03 +0700

    5.5 checkpoint

--- main.js
+++ main.js
@@ -7,14 +7,32 @@ createApp({
         const url = ref ('https://www.camt.cmu.ac.th'); 
         const inventory = ref(11);
         const onSale = ref(true);
+        const details = ref([
+            '50% cotton', 
+            '30% wool', 
+            '20% polyester'
+        ]);
+        const variants = ref([
+            {
+                id: 2234,
+                color: 'green',
+            },
+            {
+                id:2235,
+                color: 'blue',
+            }
+        ])
+        const sizes = ref(["S", "M", "L"]);
         
         return{
             product,
             image,
             url,
             inventory,
-            onSale
-
+            onSale,
+            details,
+            variants,
+            sizes
         }
     }
 }).mount('#app');
